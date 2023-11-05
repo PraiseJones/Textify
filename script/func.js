4fb0d0b44593b18645d3cf659eb9fb07110c634f
PraiseJones <praisejones18@gmail.com>
2023-11-05 15:59:04 +0100

    updated

--- script/func.js
+++ script/func.js
@@ -1,4 +1,5 @@
 const imgUpload = document.querySelector('#input_img');
+const selectbtn = document.querySelector('.default')
 
 imgUpload.addEventListener('change', function() {
     const imgName = this.files[0].name;
@@ -6,3 +7,11 @@ imgUpload.addEventListener('change', function() {
 
     imgg.innerHTML = imgName;
 })
+
+selectbtn.addEventListener('click', () => {
+    const dropDown = document.querySelector('.select');
+    dropDown.classList.toggle('active');
+
+    
+})
+
