846f4284aa9970a83132da0c0134a8664bc50190
PraiseJones <praisejones18@gmail.com>
2024-02-11 21:14:33 +0100

    responsiveness

--- script/func.js
+++ script/func.js
@@ -1,5 +1,8 @@
 const imgUpload = document.querySelector('#input_img');
 const selectbtn = document.querySelector('.default')
+const checkbox = document.getElementById("check");
+const ddIcon = document.querySelector("#dd-icon");
+const closeIcon = document.querySelector("#close-icon");
 
 imgUpload.addEventListener('change', function() {
     const imgName = this.files[0].name;
@@ -12,3 +15,20 @@ selectbtn.addEventListener('click', () => {
     const dropDown = document.querySelector('.select');
     dropDown.classList.toggle('active');
 })
+
+checkbox.addEventListener("change", () => {
+  ddIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+});
+
+closeIcon.addEventListener("click", () => {
+  const height = document.querySelector(".h-full")
+  height.classList.remove("h-96");
+  height.classList.add("h-0")
+});
+
+ddIcon.addEventListener("click", () => {
+    const height = document.querySelector(".h-0")
+    height.classList.remove("h-0");
+    height.classList.add("h-96")
+  });
